7845a919a2b1db996f3f434b7f64fa80e8954679
yqh <yangqihai997@163.com>
2023-03-30 17:19:43 +0800

    for loop demo && break continue diff?

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-const s = "constant var"
-
 func main() {
-	fmt.Println("constant")
-	fmt.Println(s)
+	fmt.Println("for")
 
-	const n = 50000000
-	const d = 3e20 / n
-	fmt.Println(d)
-	fmt.Println(int64(d))
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
 
-	fmt.Println(math.Sin(n))
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop for")
+		break
+	}
+
+	for i := 0; i <= 5; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	for i := 0; i <= 5; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+		// if break here, i=0, i++ is Unreachable code
+		break
+	}
 }
