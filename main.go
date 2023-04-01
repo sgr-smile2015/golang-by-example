17c0c10f450d4202820dc9443665017164df5519
yqh <yangqihai997@163.com>
2023-04-01 11:26:24 +0800

    slices demo

--- main.go
+++ main.go
@@ -5,25 +5,45 @@ import (
 )
 
 func main() {
-	fmt.Println("arrays")
-	var a [5]int
-	fmt.Println("zero arrays: ", a)
+	fmt.Println("slices")
 
-	a[4] = 100
-	fmt.Println("set: ", a)
-	fmt.Println("get: ", a[4])
-	fmt.Println("len(a) ", len(a))
+	s := make([]string, 3)
+	fmt.Println("initial slice: ", s)
 
-	b := [5]string{"a", "b", "c", "d", "e"}
-	fmt.Println("dcl: ", b)
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set: ", s)
+	fmt.Println("get: ", s[2])
+	fmt.Println("len:  ", len(s))
+	s = append(s, "e")
+	s = append(s, "f", "g")
+	fmt.Println("append slice: ", s)
 
-	var towD [2][3]int
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("copy s slice: ", c)
 
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 3; j++ {
+	l := s[2:5]
+	fmt.Println("slice [2:5]", l)
+	l = s[:5]
+	fmt.Println("slice [:5]", l)
+	l = s[2:]
+	fmt.Println("slice [2:]", l)
+
+	t := []string{"java", "golang", "python", "c++", "rust"}
+	fmt.Println("t: ", t)
+
+	towD := make([][]int, 3)
+
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		towD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
 			towD[i][j] = i + j
 		}
 	}
 
-	fmt.Println("tow: ", towD)
+	fmt.Println("towD: ", towD)
+
 }
